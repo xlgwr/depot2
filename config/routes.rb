714ea835937b14ad6587100008fcf4fcd79dd778
xlgwr <xlgwr@ubuntu>
2014-01-18 10:20:33 +0800

    finish admin users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Depot2::Application.routes.draw do
+  get "admin" => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+#  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
+  resources :users
+
   resources :orders
 
   resources :line_items
@@ -8,12 +19,18 @@ Depot2::Application.routes.draw do
   get "store/index"
   resources :products
 
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-   root 'store#index', as: 'store'
+#   root 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
