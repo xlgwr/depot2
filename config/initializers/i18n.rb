1d097052b296473878444b29730330bc191d4d38
xlgwr <xlgwr@ubuntu>
2014-01-18 12:51:55 +0800

    finish en es cn I18n locale

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,9 +1,9 @@
-#encoding: utf-8
+﻿#encoding: utf-8
 I18n.default_locale = :en
 
 LANGUAGES = [
   [ 'English', 'en'],
              ["Espa&ntilde;ol".html_safe, 'es'],
-             ['china', 'cn']
+             ['中国', 'cn']
 ]
    
